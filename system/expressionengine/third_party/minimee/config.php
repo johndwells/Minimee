bd9b1d125cbdc988c8892fdf65f2c20b243ca6ab
John D Wells <johndwells@gmail.com>
2012-01-10 14:29:09 +0000

    Increment version to go along with previous bug fix.

--- system/expressionengine/third_party/minimee/config.php
+++ system/expressionengine/third_party/minimee/config.php
@@ -2,7 +2,7 @@
 if (! defined('MIMIMEE_VER'))
 {
 	define('MIMIMEE_NAME', 'Minimee');
-	define('MIMIMEE_VER',  '1.1.3');
+	define('MIMIMEE_VER',  '1.1.4');
 	define('MIMIMEE_AUTHOR',  'John D Wells');
 	define('MIMIMEE_DOCS',  'http://johndwells.com/software/minimee');
 	define('MIMIMEE_DESC',  'Minimee: minimize & combine your CSS and JS files. For EE2 only.');
