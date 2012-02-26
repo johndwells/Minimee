ff3e30c8e9552bc0c22b0b08e0111eed8521d899
John D Wells <johndwells@gmail.com>
2012-02-26 22:08:38 +0000

    Tweak settings language.

--- system/expressionengine/third_party/minimee/language/english/lang.minimee.php
+++ system/expressionengine/third_party/minimee/language/english/lang.minimee.php
@@ -7,26 +7,26 @@ $lang = array(
 	'cachebust_hint' => 'e.g. `1.0.0`.',
 
 	'base_path' => 'Base Path',
-	'base_path_note' => 'The base path from which your CSS and JS files reside.<br />Optional, defaults to FCPATH constant (the root path to your site).',
+	'base_path_note' => 'The location on your webserver where your <i>source</i> CSS and JS files sit.<br />Optional, defaults to FCPATH constant (the root path to your site).',
 	'base_path_hint' => 'e.g. ' . rtrim(FCPATH, '/'),
 
 	'base_url' => 'Base URL',
-	'base_url_note' => 'The base URL from which your CSS and JS files serve.<br />Optional, defaults to Site URL.',
+	'base_url_note' => 'The base URL from which your <i>source</i> CSS and JS files are served.<br />Optional, defaults to Site URL.',
 	'base_url_hint' => 'e.g. ' . rtrim(get_instance()->config->item('base_url'), '/'),
 
 	'cache_path' => 'Cache Path',
-	'cache_path_note' => 'Relative paths (without a leading slash) will be appended to the Base Path.<br />If left blank, will guess `cache`.',
+	'cache_path_note' => 'Relative values (without a leading slash) will be appended to the Base Path.<br />If left blank, will guess `cache`.',
 	'cache_path_hint' => 'e.g. ' . rtrim(FCPATH, '/') . '/cache',
 
 	'cache_url' => 'Cache URL',
-	'cache_url_note' => 'Relative paths (without a leading slash) will be appended to the Base URL.<br />If left blank, will guess `cache`.',
+	'cache_url_note' => 'Relative values (without a leading slash) will be appended to the Base URL.<br />If left blank, will guess `cache`.',
 	'cache_url_hint' => 'e.g. ' . rtrim(get_instance()->config->item('base_url'), '/') . '/cache',
 
 	'combine' => 'Combine Assets',
 	'combine_note' => 'Specify which types of assets to combine.',
 	
 	'css_prepend_mode' => 'CSS Prepend Mode',
-	'css_prepend_mode_note' => 'By default when minifying CSS, Minimee will rewrite <strong>relative</strong> image & @import URLs into absolute URLs. Turn OFF to skip this step.',
+	'css_prepend_mode_note' => 'By default when minifying CSS, Minimee will rewrite <i>relative</i> image & @import URLs into absolute URLs. Turn OFF to skip this step.',
 
 	'css_prepend_url' => 'CSS Prepend URL',
 	'css_prepend_url_note' => 'The URL to use when `CSS Prepend Mode` is ON.<br />Optional, by default uses the Base URL.',
@@ -41,10 +41,6 @@ $lang = array(
 	'remote_mode_note' => 'Specify how to fetch remote and {stylesheet=} URLs. \'Auto\' mode will try cURL first.',
 
 	'save' => 'Save Settings',
-	'yes' => 'Yes',
-	'no' => 'No',
-	'on' => 'On',
-	'off' => 'Off',
 	'auto' => 'Auto',
 	'curl' => 'cURL',
 	'fgc' => 'file_get_contents()',
