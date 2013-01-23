a1df1430497ca8eb1dc284368e5a2948fb1abf73
John D Wells <john@onedarnleyroad.com>
2013-01-23 18:31:51 +0000

    Remove merge syntax.

--- system/expressionengine/third_party/minimee/config.php
+++ system/expressionengine/third_party/minimee/config.php
@@ -2,11 +2,7 @@
 if (! defined('MINIMEE_VER'))
 {
 	define('MINIMEE_NAME', 'Minimee');
-<<<<<<< HEAD
-	define('MINIMEE_VER',  '2.1.6');
-=======
 	define('MINIMEE_VER',  '2.1.7');
->>>>>>> refs/heads/master
 	define('MINIMEE_AUTHOR',  'John D Wells');
 	define('MINIMEE_DOCS',  'http://johndwells.github.com/Minimee');
 	define('MINIMEE_DESC',  'Minimee: minimize & combine your CSS and JS files. Minify your HTML. For EE2 only.');
