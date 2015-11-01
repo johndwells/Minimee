098a8644d5e973fcfeb7377d03ae649e03dbff23
John D Wells <john@onedarnleyroad.com>
2015-11-01 13:27:28 +0000

    Bump version, introduce addon.setup.php.

---
+++ system/user/addons/minimee/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once PATH_THIRD . 'minimee/config.php';
+
+return array(
+	'name'          => MINIMEE_NAME,
+	'version'       => MINIMEE_VER,
+	'author'        => MINIMEE_AUTHOR,
+	'author_url'    => MINIMEE_DOCS,
+	'description'   => MINIMEE_DESC,
+	'namespace'     => MINIMEE_NAME
+);
--- system/user/addons/minimee/config.php
+++ system/user/addons/minimee/config.php
@@ -2,10 +2,10 @@
 if (! defined('MINIMEE_VER'))
 {
 	define('MINIMEE_NAME', 'Minimee');
-	define('MINIMEE_VER',  '2.2.0');
+	define('MINIMEE_VER',  '3.0.0');
 	define('MINIMEE_AUTHOR',  'John D Wells');
 	define('MINIMEE_DOCS',  'http://johndwells.github.com/Minimee');
-	define('MINIMEE_DESC',  'Minimee: minimize & combine your CSS and JS files. Minify your HTML. For EE2 only.');
+	define('MINIMEE_DESC',  'Minimee: minimize & combine your CSS and JS files. Minify your HTML. For EE2 and EE3.');
 }
 
 $config['name'] = MINIMEE_NAME;
